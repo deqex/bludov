bf6ff723e372ae039e2c3f3d9bccc8360ca968de
handlon <jan.handlik@gmail.com>
2024-05-14 19:53:36 +0200

    home ma css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,63 +1,12 @@
 import { useState, useEffect, useRef } from "react"
-
+import './Home.css'
 export default function Home() {
 
 
-    const [firstBox, setFirstBox] = useState(false);
-    const [secondBox, setSecondBox] = useState(false);
-    const [thirdBox, setThirdBox] = useState(false);
-    const heading = useRef();
-
-    const handleCheckbox = (e) => {
-
-
-
-        switch (e.target.name) {
-            case "Tomas":
-                setFirstBox(!firstBox);
-                break;
-            case "Franta":
-                setSecondBox(!secondBox);
-                break;
-            case "Pepa":
-                setThirdBox(!thirdBox);
-                break;
-
-        }
-
-    }
-
-
-
-    useEffect(() => {
-        document.title = "page loded"
-        heading.current.style.color = "red"
-
-    }, []);
-
-    useEffect(() => {
-        heading.current.style.color = "blue"
-    }, [firstBox])
-
-    useEffect(() => {
-        heading.current.style.color = "orange"
-    }, [secondBox])
-
-    useEffect(() => {
-        heading.current.style.color = "green"
-    }, [thirdBox])
-
+    
     return (
         <>
-            <h1 ref={heading}>home</h1>
-
-            <input type="checkbox" name="Tomas" onChange={handleCheckbox} />
-            <input type="checkbox" name="Franta" onChange={handleCheckbox} />
-            <input type="checkbox" name="Pepa" onChange={handleCheckbox} />
-
-            <p>Tomas: {firstBox + ""}</p>
-            <p>Franta: {secondBox + ""}</p>
-            <p>Pepa : {thirdBox + ""}</p>
+            
 
 
         </>
