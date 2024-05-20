ea55f39c8979b2ad779882eb0ee350b8cb9d1eae
handlon <jan.handlik@gmail.com>
2024-05-20 13:39:11 +0200

    jdsfgh

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-
+import Navbar from '../assets/components/Navbar'
 import './Home.css'
 export default function Home() {
 
@@ -7,7 +7,8 @@ export default function Home() {
     return (
         <>
             
-
+        <Navbar></Navbar>
+        <h1>jsi restrat</h1>
 
         </>
     )
