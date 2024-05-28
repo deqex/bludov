cc2c969e4c00ab2029d7ddfe9e27c8b0d9c70fd2
deqex <depexpubg@gmail.com>
2024-05-28 14:25:19 +0200

    oobci

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Home from "./pages/Home";
 import Handlik from "./pages/Handlik/Handlik";
 import Depex from "./pages/Depex/Depex";
+import OObci from "./pages/OObci/OObci";
+
 
 
 export default function AppRoutes() {
@@ -15,6 +17,7 @@ export default function AppRoutes() {
                     <Route path="/" element={<Home></Home>} />
                     <Route path="/handlik" element={<Handlik></Handlik>} />
                     <Route path="/Depex" element={<Depex></Depex>} />
+                    <Route path="/OOBci" element={<OObci></OObci>} />
                 </Routes>
             </BrowserRouter>
         </>
---
+++ src/pages/OObci/OObci.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TextAobr from '../../assets/components/TextAobr/TextAobr'
+import Navbar from '../../assets/components/Navbar/Navbar'
+
+export default function OObci() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <TextAobr></TextAobr>
+    </>
+  )
+}
