0dbaded457bb698b9a5b8097fe63155972249695
Handlon <jan.handlik@gmail.com>
2024-05-28 14:59:21 +0200

    obr big je done

--- src/pages/Handlik/Handlik.jsx
+++ src/pages/Handlik/Handlik.jsx
@@ -2,11 +2,14 @@ import TextArea from '../../assets/components/TextArea/TextArea'
 import AkceThumbnail from '../../assets/components/AkceThumbnail/AkceThumbnail'
 import Footer from '../../assets/components/Footer/Footer'
 import TextAobr from '../../assets/components/TextAobr/TextAobr'
+import BIG from '../../assets/components/BIG/BIG'
 import './Handlik.css'
 
 export default function Handlik() {
   return (
     <div className='body'>
+
+      <BIG></BIG>
       <div className='textareacontainer'>
 
 
